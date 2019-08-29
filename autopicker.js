5531514c094dd278bd93f47c83efe023906d0cf5
queviva <jabraham.engineer@gmail.com>
2019-08-29 15:19:42 -0700

    touch sucks in all mobile devices

--- autopicker.js
+++ autopicker.js
@@ -239,17 +239,17 @@ const AutoPicker = function (selectors='.autopicker') {
             this.togAbility = v =>  v ? able : (obj[((able = (able + 1) % 2) ? 'add' : 'remove') + 'EventListener']('wheel', lizzer, { passive: false }), able);
             
             // debuGG
-            obj.innerHTML = 'lArded';
+            obj.innerHTML = 'lenderf';
                 
-            obj.addEventListener('mousedown', e => {
+            obj.addEventListener('touchstart', e => {
                 console.log('starting');
-                obj.addEventListener('mousemove', pointMover, {passive: false});
+                obj.addEventListener('touchmove', pointMover, {passive: false});
                 looping = setTimeout(anim, 500);
             });
             
-            obj.addEventListener('mouseup', e => {
+            obj.addEventListener('touchend', e => {
                 console.log('stopping');
-                obj.removeEventListener('mousemove', pointMover);
+                obj.removeEventListener('touchmove', pointMover);
                 looping = (clearTimeout(looping));
                 console.log(looping, looping ? 'yes' : 'no')
             });
