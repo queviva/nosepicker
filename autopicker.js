b69a43612f018abfe7cece14e2d95cb716bb5205
queviva <jabraham.engineer@gmail.com>
2020-02-06 16:06:40 -0800

    all three versions, wheel|touch|coast functional - will still try to optimize later

--- autopicker.js
+++ autopicker.js
@@ -4,7 +4,7 @@
  */
  
  
-const AutoPicker = function (selectors='.nosepicker') {
+const AutoPicker = function (selectors = '.nosepicker') {
     
     const
     
@@ -12,7 +12,7 @@ const AutoPicker = function (selectors='.nosepicker') {
     
     clam = (...x) => x.sort((a, b) => a - b)[1],
     
-    setVals = (nose, x, v=nose.hsla) => {
+    setVals = (nose, x, v = nose.hsla) => {
     
         nose.value = DIV.style.color = x;
         
@@ -22,8 +22,7 @@ const AutoPicker = function (selectors='.nosepicker') {
         max = Math.max(r, g, b),
         min = Math.min(r, g, b),
         d = max - min,
-        dX = d !== 0;//,
-        //v = nose.hsla;
+        dX = d !== 0;
         
         [v.h, v.s, v.l, v.a] =
         [
@@ -41,66 +40,164 @@ const AutoPicker = function (selectors='.nosepicker') {
         
             let
             
-            able = 0, prev = { X: 0, Y: 0 },
-            
-            msgs = {
-                loaded: obj.dataset.noseLoadedEventName || 'noseloaded',
-                input: obj.dataset.noseInputEventName || 'noseinput',
-                colorsrc: obj.dataset.noseColorSrc || 'background-color'
+            prefs = {
+                kind: 'touch', // touch|wheel|coast
+                colorsrc: 'background-color',
+                loadedEventName: 'noseloaded',
+                inputEventName: 'noseinput',
             },
-
-            lizzer = (
-                e,
-                //x = e.preventDefault(),
-                v = this.hsla,
-                [dx, dy] = [
-                    (2.5/(e.shiftKey ? 500 : 50)) * (prev.X - e.touches[0].pageX),
-                    (5/(e.shiftKey ? 500 : 50)) * (prev.Y - e.touches[0].pageY),
-                ]
-            ) => {
-                prev = { X: e.touches[0].pageX, Y: e.touches[0].pageY };
+    
+            nosePrefs = obj.dataset.nose ? JSON.parse(obj.dataset.nose) : {},
+    
+            able = 1,
+    
+            prev = { X: 0, Y: 0 },
+    
+            looping = false,
+    
+            lizzer = (e, [dx, dy], CTRL, v = this.hsla) => {
+    
                 e.stopPropagation();
                 e.preventDefault();
-                obj.dispatchEvent(new CustomEvent(msgs.input, {
-            
+    
+                obj.dispatchEvent(new CustomEvent(prefs.inputEventName, {
+    
                     detail: {
                         vals: [v.h, v.s, v.l, v.a] =
-                            e.touches[1] ?
-                                [v.h, clam(v.s + dy, 100, 0), v.l, clam(v.a - dx * 0.01, 1, 0)]
-                            :
-                                [v.h - dy, v.s, clam(v.l + dx, 100, 0), v.a],
+                            CTRL ? [v.h, clam(v.s + dy, 100, 0), v.l, clam(v.a - dx * 0.01, 1, 0)] :
+                            [v.h - dy, v.s, clam(v.l + dx, 100, 0), v.a],
                         hsla: v,
                         value: (this.value = 'hsla(' + v.h + ',' + v.s + '%,' + v.l + '%,' + v.a + ')')
-            
+    
                     }
-            
+    
                 }));
+    
+            },
+    
+            wheelLizzer = e => {
+    
+                lizzer(
+                    e,
+                    [
+                        ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaX),
+                        ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaY),
+                    ],
+                    e.ctrlKey
+                );
+    
+            },
+    
+            touchLizzer = e => {
+                
+                lizzer(
+                    e,
+                    [
+                        0.1 * (prev.X - e.touches[0].pageX),
+                        0.1 * (prev.Y - e.touches[0].pageY)
+                    ],
+                    e.touches[1]
+                );
+    
+                prev = {
+                    X: e.touches[0].pageX,
+                    Y: e.touches[0].pageY
+                };
+    
+            },
+    
+            coastInc = [0, 0],
+            
+            coastLizzer = e => {
+                coastInc = [
+                    0.01 * (prev.X - e.touches[0].pageX),
+                    0.05 * (prev.Y - e.touches[0].pageY)
+                ];
+            },
+            
+            coastLooper = e => {
+                if (looping) {
+    
+                    lizzer(e, coastInc, e.touches[1]);
+                    setTimeout(() => coastLooper(e), 50);
+    
+                }
+    
+            },
+            
+            kindList = {
+                wheel : { wheel : wheelLizzer },
+                touch : {
+                    touchmove : touchLizzer,
+                    touchstart : e => prev = {
+                        X: e.touches[0].pageX,
+                        Y: e.touches[0].pageY
+                    }
+                },
+                coast : {
+                    touchmove : coastLizzer,
+                    touchend : e => looping = clearTimeout(looping),
+                    touchstart : e => {
+                    
+                        prev = {
+                            X: e.touches[0].pageX,
+                            Y: e.touches[0].pageY
+                        };
+                    
+                        looping = setTimeout(() => coastLooper(e), 200);
+                    
+                    }
+                }
             };
             
-            obj.addEventListener('touchstart', e => {
-                
-                prev = {
-                    X : e.touches[0].pageX,
-                    Y : e.touches[0].pageY
+            for (let i in nosePrefs) {
+                if (prefs[i]) {
+                    prefs[i] = nosePrefs[i];
                 }
-                
-            }, {passive: false});
-                
+            }
+            
             this.hsla = {};
             
-            this.value = window.getComputedStyle(obj)[msgs.colorsrc];
+            this.value = window.getComputedStyle(obj)[prefs.colorsrc];
             
             this.setValue = val => setVals(this, val);
             
-            this.togAbility = v => v ? able : (obj[((able = (able + 1) % 2) ? 'add' : 'remove') + 'EventListener']('touchmove', lizzer, { passive: false }), able);
+            this.togAbility = v => {
                 
-            obj.dispatchEvent(new CustomEvent(msgs.loaded, {
+                if(v) {
+                    
+                    return able;
+                    
+                } else {
+                    
+                    let dodo = ['add', 'remove'][(able = (able + 1) % 2)];
+                    let msg = '';
+                    
+                    for (let evt in kindList[prefs.kind]) {
+                        obj[dodo + 'EventListener'](
+                            evt,
+                            kindList[prefs.kind][evt],
+                            { passive: false }
+                        );
+                        msg += [dodo, evt, 'on', obj.id, '\n'].join(' ');
+                    }
+                    
+                    return msg;
+            
+                }
+                    
+            };
+            
+            obj.dispatchEvent(new CustomEvent(prefs.loadedEventName, {
                 detail: {
                     value: setVals(this, this.value),
                     ability: this.togAbility()
-                    ,DEBUGG: //(console.log(
-                        obj.id + ' loaded ' +  obj.getBoundingClientRect()
-                    //)
+                    /*
+                    ,DEBUGG: (console.log(
+                        obj.id + ' kind: ' + prefs.kind, '\n',
+                        (prefs.loadedEventName)
+                    ))
+                    //*/
                 }
             }));
             
--- nosepicker.js
+++ nosepicker.js
@@ -1,98 +1,18 @@
 /**
  * @author pizzaface
  * @version 1.0
- *
- * @fileOverview NosePicker object/namespace that
- *     holds values for all the elements in the page
- *     that you want to be nosepickers - access
- *     them through their html id, and listen for
- *     either the <i>noseinput</i> event [or specify
- *     some custom event name] - scroll UP|DOWN to
- *     set the hue, scroll LEFT|RIGHT to set the saturation,
- *     CTRL+scroll LEFT|RIGHT sets the transparency
- *     CTRL+scroll UP|DOWN set the luminocity
- *     holding the SHIFT key will increase the sensitivity
- *
- * @typedef {Object} nosepicker - hsla picker from scroll|wheelies
- *
  */
  
- /**
-  * @example
-  *
-  * <div id="picker"></div>
-  *
-  * <svg><g><ellipse
-  *     id="svgPicker"
-  *     class="svgShape"
-  *     data-nose-color-src="fill"
-  *     data-nose-input-event-name="picked"
-  *     cx="100" cy="100" rx="100" ry="100"
-  * /></g></svg>
-  *
-  * <script>
-  *
-  * // makes nosepickers out of elements that have
-  * // the id <i>picker</i>, or are <i>ellipse</i>
-  * // children of an svg <i>g</i> element
-  * const NP = new NosePicker('#picker, g > ellipse');
-  *
-  * // picker is a default example
-  * // handle its <i>noseinput</i> event
-  * picker.addEventListener('noseinput', e => {
-  *     document.body.style.color = NP.picker.value;
-  * });
-  *
-  * // svgPicker is customized
-  * // its <i>fill</i> value gets used to set the nosepicker value
-  * // handle its custome <i>picked</i> event
-  * svgPicker.addEventListener('picked', e => {
-  *
-  *     // set a color from the nosepicker obj's value
-  *     someDiv.style.backgroundColor = NP.svgPicker.value;
-  *
-  *     // or from the event's detail value
-  *     otherDiv.style.color = e.detail.value;
-  *
-  *     // access the individual hsla values from .hsla
-  *     someDiv.style.color = NP.svgPicker.hsla.l > 50 ? '#000' : '#fff';
-  *
-  *
-  * });
-  *
-  * </script>
-  *
-
-/**
- * @constructor
- * @param {string} [selectors=.nosepicker] a valid
- *     css selector string listing everything that
- *     will be converted into a nosepicker
- */
-const NosePicker = function (selectors='.nosepicker') {
+ 
+const AutoPicker = function (selectors = '.nosepicker') {
     
     const
     
-    /** @constant - hakc div for retrieving converted color strings */
     DIV = document.createElement('div'),
     
-    /**
-     * will gate a value between specified min|max values
-     * @param {number[]} destructured array of three values
-     *     you can pass more than three values, but that
-     *     will not produce the desired result
-     */
     clam = (...x) => x.sort((a, b) => a - b)[1],
     
-    /**
-     * @desc NosePicker's own way of computing and assigning
-     *     color values to a nosepicker object
-     *
-     * @param {nosepicker} nose - the obj whose value should be set
-     * @param {string} x - any valid css color string
-     *
-     */
-    setVals = (nose, x, v=nose.hsla) => {
+    setVals = (nose, x, v = nose.hsla) => {
     
         nose.value = DIV.style.color = x;
         
@@ -102,8 +22,7 @@ const NosePicker = function (selectors='.nosepicker') {
         max = Math.max(r, g, b),
         min = Math.min(r, g, b),
         d = max - min,
-        dX = d !== 0;//,
-        //v = nose.hsla;
+        dX = d !== 0;
         
         [v.h, v.s, v.l, v.a] =
         [
@@ -115,114 +34,170 @@ const NosePicker = function (selectors='.nosepicker') {
     
     };
 
-    // loop through all the nosepicker elements
     document.querySelectorAll(selectors).forEach(n => this[n.id] =
         
-        /**
-         * @constructor make an AnonymObject for each element
-         *     this slash-quote <i>weird</i> quothe slathsch
-         *     construction IS, infact, necessary
-         */
         new (function(obj) {
         
             let
             
-            /** @type {number} zero|one; is the picker enabled? */
-            able = 0,
-            
-            /**
-             * @type {object}
-             * @property {string} [loaded=noseloaded] the load event name
-             * @property {string} [input=noseinput] the input event name
-             * @property {string} [colorsrc=background-color] where
-             *     NosePicker should retrieve the initial value from;
-             *     if you want the text color or the fill color, or
-             *     the stroke color, for example
-            */
-            msgs = {
-                loaded: obj.dataset.noseLoadedEventName || 'noseloaded',
-                input: obj.dataset.noseInputEventName || 'noseinput',
-                colorsrc: obj.dataset.noseColorSrc || 'background-color'
+            prefs = {
+                kind: 'touch', // touch|wheel|coast
+                colorsrc: 'background-color',
+                loadedEventName: 'noseloaded',
+                inputEventName: 'noseinput',
             },
-            
-            /**
-             * @desc the scroll|wheelie event listener
-             * @returns {object} e.detail - this is a CustomEvent
-             *     dispatching handler - the event's name is
-             *     either <i>noseinput</i> or set in the html
-             *     data-nose-input-event-name="customeName"
-             *     attribute
-             */
-            lizzer = (
-                e,
-                x = e.preventDefault(),
-                v = this.hsla,
-                [sk, dx, dy] = [e.shiftKey ? 500 : 50, e.wheelDeltaX, e.wheelDeltaY]
-            ) => {
+    
+            nosePrefs = obj.dataset.nose ? JSON.parse(obj.dataset.nose) : {},
+    
+            able = 1,
+    
+            prev = { X: 0, Y: 0 },
+    
+            looping = false,
+    
+            lizzer = (e, [dx, dy], CTRL, v = this.hsla) => {
+    
                 e.stopPropagation();
                 e.preventDefault();
-                obj.dispatchEvent(new CustomEvent(msgs.input, {
-            
-                    /**
-                     * @type {object} event's .detail value
-                     * @property {object} vals - h.s.l.a values
-                     * @property {object} hsla - the h.s.l.a values again
-                     * @property {string} value - a css valid hsla() color
-                    */
+    
+                obj.dispatchEvent(new CustomEvent(prefs.inputEventName, {
+    
                     detail: {
-                        vals: [v.h, v.s, v.l, v.a] = e.ctrlKey ? [v.h, clam(v.s + dy / sk, 100, 0), v.l, clam(v.a - dx / sk * 0.01, 1, 0)] : [v.h - dy / sk, v.s, clam(v.l + dx / sk, 100, 0), v.a],
+                        vals: [v.h, v.s, v.l, v.a] =
+                            CTRL ? [v.h, clam(v.s + dy, 100, 0), v.l, clam(v.a - dx * 0.01, 1, 0)] :
+                            [v.h - dy, v.s, clam(v.l + dx, 100, 0), v.a],
                         hsla: v,
                         value: (this.value = 'hsla(' + v.h + ',' + v.s + '%,' + v.l + '%,' + v.a + ')')
-            
+    
                     }
-            
+    
                 }));
-            };
+    
+            },
+    
+            wheelLizzer = e => {
+    
+                lizzer(
+                    e,
+                    [
+                        ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaX),
+                        ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaY),
+                    ],
+                    e.ctrlKey
+                );
+    
+            },
+    
+            touchLizzer = e => {
                 
-            /** @type {object} exposed holder for individual h.s.l.a values */
+                lizzer(
+                    e,
+                    [
+                        0.1 * (prev.X - e.touches[0].pageX),
+                        0.1 * (prev.Y - e.touches[0].pageY)
+                    ],
+                    e.touches[1]
+                );
+    
+                prev = {
+                    X: e.touches[0].pageX,
+                    Y: e.touches[0].pageY
+                };
+    
+            },
+    
+            coastInc = [0, 0],
+            
+            coastLizzer = e => {
+                coastInc = [
+                    0.01 * (prev.X - e.touches[0].pageX),
+                    0.05 * (prev.Y - e.touches[0].pageY)
+                ];
+            },
+            
+            coastLooper = e => {
+                if (looping) {
+    
+                    lizzer(e, coastInc, e.touches[1]);
+                    setTimeout(() => coastLooper(e), 50);
+    
+                }
+    
+            },
+            
+            kindList = {
+                wheel : { wheel : wheelLizzer },
+                touch : {
+                    touchmove : touchLizzer,
+                    touchstart : e => prev = {
+                        X: e.touches[0].pageX,
+                        Y: e.touches[0].pageY
+                    }
+                },
+                coast : {
+                    touchmove : coastLizzer,
+                    touchend : e => looping = clearTimeout(looping),
+                    touchstart : e => {
+                    
+                        prev = {
+                            X: e.touches[0].pageX,
+                            Y: e.touches[0].pageY
+                        };
+                    
+                        looping = setTimeout(() => coastLooper(e), 200);
+                    
+                    }
+                }
+            };
+            
+            for (let i in nosePrefs) {
+                if (prefs[i]) {
+                    prefs[i] = nosePrefs[i];
+                }
+            }
+            
             this.hsla = {};
             
-            /** @type {string} exposed css color string value */
-            this.value = window.getComputedStyle(obj)[msgs.colorsrc];
+            this.value = window.getComputedStyle(obj)[prefs.colorsrc];
             
-            /**
-             * @desc exposed method for directly setting the nosepicker's
-             *     color value
-             * @param {string} val - takes any valid css color value
-             */
             this.setValue = val => setVals(this, val);
             
-            /**
-             * @desc toggles the disability of the nosepicker
-             *
-             * @param {string|number|boolean} v - if any value gets
-             *     passed in, which can be evaluated to <i>true</i>
-             *     from "v ?" then the current ability will be
-             *     returned - ELSE the current ability will be
-             *     toggled between zero|one, and then the ability's
-             *     state returned
-             * @returns {int|boolean} zero|one; is the picker abled?
-             *
-             * @example
-             *
-             * NP.picker0.togAbility('anything'); // returns 0|1
-             * NP.picker0.togAbility(); // toggles picker0's ability
-             *
-             */
-            this.togAbility = v => v ? able : (obj[((able = (able + 1) % 2) ? 'add' : 'remove') + 'EventListener']('wheel', lizzer, { passive: false }), able);
+            this.togAbility = v => {
                 
-            obj.dispatchEvent(new CustomEvent(msgs.loaded, {
-                /**
-                 * @type {object} the event's .detail from the
-                 *     loading event - default name is <i>noseload</i>
-                 *     can bet set using the html data-attribute
-                 *     data-nose-loaded="customeLoadName"
-                 * @property {undefined} value - sets the picker's value
-                 * @property {int|boolean} ability - 0|1 ; is the picker able?
-                 */
+                if(v) {
+                    
+                    return able;
+                    
+                } else {
+                    
+                    let dodo = ['add', 'remove'][(able = (able + 1) % 2)];
+                    let msg = '';
+                    
+                    for (let evt in kindList[prefs.kind]) {
+                        obj[dodo + 'EventListener'](
+                            evt,
+                            kindList[prefs.kind][evt],
+                            { passive: false }
+                        );
+                        msg += [dodo, evt, 'on', obj.id, '\n'].join(' ');
+                    }
+                    
+                    return msg;
+            
+                }
+                    
+            };
+            
+            obj.dispatchEvent(new CustomEvent(prefs.loadedEventName, {
                 detail: {
                     value: setVals(this, this.value),
                     ability: this.togAbility()
+                    /*
+                    ,DEBUGG: (console.log(
+                        obj.id + ' kind: ' + prefs.kind, '\n',
+                        (prefs.loadedEventName)
+                    ))
+                    //*/
                 }
             }));
             
