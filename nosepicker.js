4b1524c41ad1867fbb93c8da4172dbe384cf64d3
queviva <jabraham.engineer@gmail.com>
2020-02-21 14:24:49 -0800

    version three now allows get/set of values from a created reference, optionally added to the object, or to be returned from a dispatched event call - this is pretty good, but still has the impossible to werk around non-object-oriented lizzers

--- nosepicker.js
+++ nosepicker.js
@@ -1,44 +1,54 @@
 /**
  * @author pizzaface
- * @version 2.0
+ * @version 3.0
  */
 
-// window load closure
-window.addEventListener('load', () => { new (function () {
+
+// weird _new construction_ IS necessary, thank you
+new (function () {
     
-    const opts = JSON.parse(((document.querySelector(
-         'script[src*="nosepicker"][src$=".js"]'
-    ) || {}).dataset || {}).nose|| '{}');
+    'use strict';
     
+    // the default preferences
     const defPrefs = {
-        selector : 'nosepicker',
-        makeRef : false,
+        selector: 'picker',
+        makeRef: false,
         kind: 'touch,wheel', // wheel|touch|swipe|coast|point
         colorsrc: 'background-color',
         loadedEventName: 'noseloaded',
         inputEventName: 'noseinput',
         able: true,
         colorSelf: true,
+        colorText: true,
         transPattern: false,
         sens: { X: 1, Y: 1 },
         lizzList: {}
     };
+    
+    // grab any options that over-rite defaults
+    const opts = JSON.parse(((document.querySelector(
+        'script[src*="nosepicker"][src$=".js"]'
+    ) || {}).dataset || {}).nose || '{}');
 
-    for (let p in opts) { // set any default prefs passed in
-        if(defPrefs[p] !== undefined) defPrefs[p] = opts[p];
+    // set any valid opts passed in data-param
+    for (let p in opts) {
+        if (defPrefs[p] !== undefined) defPrefs[p] = opts[p];
     }
-    
+
+    // all the helpful methods
     const //{
-    
+
+    // award-winning method to gate a value
     clam = (...x) => x.sort((a, b) => a - b)[1],
-    
+
+    // the over-all listener for event handling
     lizzer = (e, nose, dx, dy, CTRL, v = nose.hsla) => {
-    
+
         e.stopPropagation();
         e.preventDefault();
-    
+
         nose.obj.dispatchEvent(new CustomEvent(nose.prefs.inputEventName, {
-    
+
             detail: {
                 vals: [v.h, v.s, v.l, v.a] = CTRL ? [
                     v.h,
@@ -51,9 +61,9 @@ window.addEventListener('load', () => { new (function () {
                     clam(v.l + dx, 100, 0),
                     v.a
                 ],
-    
+
                 hsla: v,
-    
+
                 value: (nose.value =
                     'hsla(' +
                     v.h + ',' +
@@ -61,106 +71,106 @@ window.addEventListener('load', () => { new (function () {
                     v.l + '%,' +
                     v.a +
                     ')')
-    
+
             }
-    
+
         }));
-    
+
     },
 
     wheelLizzer = (e, nose) => {
 
-            lizzer(
-                e, nose,
-                nose.prefs.sens.X * ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaX),
-                nose.prefs.sens.Y * ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaY),
-                e.ctrlKey
-            );
+        lizzer(
+            e, nose,
+            nose.prefs.sens.X * ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaX),
+            nose.prefs.sens.Y * ((e.shiftKey ? 0.001 : 0.01)) * (e.wheelDeltaY),
+            e.ctrlKey
+        );
 
     },
 
     touchLizzer = (e, nose) => {
-    
+
         lizzer(
             e, nose,
             nose.prefs.sens.X * 0.1 * (nose.prev.X - e.touches[0].clientX),
             nose.prefs.sens.Y * 0.1 * (nose.prev.Y - e.touches[0].clientY),
             e.touches[1]
         );
-    
+
         nose.prev = {
             X: e.touches[0].clientX,
             Y: e.touches[0].clientY
         };
-    
+
     },
 
     swipeLizzer = (e, nose) => {
-    
+
         lizzer(
             e, nose,
             nose.prefs.sens.X * 0.1 * (nose.prev.X - e.clientX),
             nose.prefs.sens.Y * 0.1 * (nose.prev.Y - e.clientY),
             e.ctrlKey
         );
-    
+
         nose.prev = {
             X: e.clientX,
             Y: e.clientY
         };
-    
+
     },
 
     coastLizzer = (e, nose) => {
-    
+
         e.preventDefault();
         e.stopPropagation();
-    
+
         nose.coastInc = [
             nose.prefs.sens.X * 0.01 * (nose.prev.X - e.touches[0].clientX),
             nose.prefs.sens.Y * 0.10 * (nose.prev.Y - e.touches[0].clientY)
         ];
-    
+
     },
 
     coastLooper = (e, nose) => {
-    
+
         e.preventDefault();
         e.stopPropagation();
-    
+
         if (nose.looping) {
             lizzer(e, nose, ...nose.coastInc, e.touches[1]);
             setTimeout(() => coastLooper(e, nose), 50);
         }
-        
+
     },
 
     pointLizzer = (e, nose) => {
-    
+
         e.preventDefault();
         e.stopPropagation();
-    
+
         nose.coastInc = [
             nose.prefs.sens.X * 0.01 * (nose.prev.X - e.clientX),
             nose.prefs.sens.Y * 0.10 * (nose.prev.Y - e.clientY)
         ];
-    
+
     },
 
     pointLooper = (e, nose) => {
-    
+
         e.preventDefault();
         e.stopPropagation();
-        
+
         if (nose.looping) {
             lizzer(e, nose, ...nose.coastInc, e.ctrlKey);
             nose.looping = setTimeout(() => pointLooper(e, nose), 50);
         }
-        
+
     },
 
     kindList = (nose, k) => {
-        
+
         return {
             wheel: {
                 wheel: e => wheelLizzer(e, nose)
@@ -174,77 +184,77 @@ window.addEventListener('load', () => { new (function () {
             },
             swipe: {
                 mousedown: e => {
-    
+
                     let slizzer = e => swipeLizzer(e, nose);
-    
+
                     window.addEventListener('mousemove', slizzer);
                     window.addEventListener('mouseup', e =>
                         window.removeEventListener('mousemove', slizzer)
                     );
-    
+
                     nose.prev = {
                         X: e.clientX,
                         Y: e.clientY
                     };
-    
+
                 },
             },
             coast: {
                 touchmove: e => coastLizzer(e, nose),
                 touchend: e => nose.looping = clearTimeout(nose.looping),
                 touchstart: e => {
-    
+
                     nose.prev = {
                         X: e.touches[0].clientX,
                         Y: e.touches[0].clientY
                     };
-    
+
                     nose.coastInc = [0, 0];
-    
+
                     nose.looping = setTimeout(() => coastLooper(e, nose), 200);
-    
+
                 }
             },
             point: {
                 mousedown: e => {
-    
+
                     let plizzer = e => pointLizzer(e, nose);
-    
+
                     window.addEventListener('mousemove', plizzer);
-    
+
                     window.addEventListener('mouseup', e => {
                         window.removeEventListener('mousemove', plizzer);
                         nose.looping = clearTimeout(nose.looping);
                     });
-    
+
                     nose.prev = {
                         X: e.clientX,
                         Y: e.clientY
                     };
-    
+
                     nose.coastInc = [0, 0];
-    
+
                     nose.looping = setTimeout(() => pointLooper(e, nose), 200);
-    
+
                 }
             }
         }[k];
-        
+
     };
 
     //}
-    
-    const NPO = function (obj) {
-        
+
+    // the NosePicker Object itself
+    const NPO = function(obj) {
+
         this.prefs = {};
-        
+
         let dataPrefs = obj.dataset.nose ? JSON.parse(obj.dataset.nose) : {};
-        
-        // if any valid prefs were given, set them
+
         for (let p in defPrefs) {
             this.prefs[p] = dataPrefs[p] ? dataPrefs[p] : defPrefs[p];
         }
- 
+
         this.obj = obj;
 
         this.hsla = {};
@@ -254,23 +264,24 @@ window.addEventListener('load', () => { new (function () {
         this.coastInc = [0, 0],
 
         this.prev = { X: 0, Y: 0 };
-        
-        this.value = window.getComputedStyle(obj)[this.prefs.colorsrc];
-        
+
+        this.value = window.getComputedStyle(obj)[this.prefs.colorsrc] || '#000';
+
         if (this.prefs.colorSelf) {
             obj.addEventListener(this.prefs.inputEventName, e => {
                 let v = e.detail.hsla;
-                obj.style.backgroundColor = e.detail.value
-                obj.style.color = v.l > 55 || v.a < 0.85 ? '#000' : '#fff';
+                obj.style[this.prefs.colorsrc] = e.detail.value;
+                if (this.prefs.colorText) obj.style.color = e.detail.hsla.l > 55 || e.detail.hsla.a < 0.85 ? '#000' : '#fff';
+                
             });
         }
-    
+
         if (this.prefs.transPattern) {
-    
+
             obj.addEventListener(this.prefs.inputEventName, e => {
-    
+
                 let A = 0.5 - e.detail.hsla.a / 2;
-    
+
                 obj.style.backgroundImage = e.detail.hsla.a < 1 ?
                     `repeating-linear-gradient(
                         -45deg,
@@ -279,90 +290,128 @@ window.addEventListener('load', () => { new (function () {
                         transparent 10px,
                         transparent 20px
                     )` : '';
-    
+
             });
-    
+
         }
-    
-        if (this.prefs.makeRef) {
-            obj[this.prefs.makeRef] = this;
-        }
-        
-        obj.dispatchEvent(new CustomEvent(this.prefs.loadedEventName, {
-            detail: {
-                value: this.setValue(this.value),
-                kind: this.setKind(this.prefs.kind)
-            }
-        }));
-        
+
     };
 
-    //protos{
-    NPO.prototype.setValue= function (v) {
-    
-        let
+    // protos {
+    NPO.prototype.loadComplete = function () {
         
+        this.obj.dispatchEvent(new CustomEvent(this.prefs.loadedEventName, {
+            detail : {
+                value : this.setValue(this.value),
+                kind : this.setKind(this.prefs.kind),
+                ref : (this.prefs.makeRef) ? this.obj[this.prefs.makeRef] = this.createRef() : ''
+            }
+        }));
+
+    };
+    NPO.prototype.createRef = function () {
+    
+        let NPO = this;
+        
+        return {
+    
+            get value() { return NPO.value },
+            set value(v) { NPO.setValue(v) },
+            get kind() { return NPO.prefs.kind.join(',') },
+            set kind(v) { NPO.setKind(v) },
+            get sens() { return NPO.prefs.sens },
+            set sens(v) { NPO.setSens(v) },
+            get able() { return NPO.prefs.able },
+            set able(v) { NPO.setAble(v) },
+            get hsla() { return NPO.hsla },
+            togAble: () => { NPO.togAble() }
+    
+        };
+    
+    };
+    NPO.prototype.setValue = function (v) {
+
         // this hack allows hsla conversion of ANY valid css color
-        DIV = document.createElement('div');
+        let DIV = document.createElement('div');
         DIV.style.color = v;
         this.value = DIV.style.color;
         
-        [r,g,b,a] = (this.value).match(/\d+\.*\d*/g).map(v => v /= 255),
-        max = Math.max(r, g, b),
-        min = Math.min(r, g, b),
-        d = max - min,
-        dX = d !== 0;
-        
-        this.hsla =
-        {
+        let
+
+        [r, g, b, a] = (this.value).match(/\d+\.*\d*/g).map(v => v /= 255),
+            max = Math.max(r, g, b),
+            min = Math.min(r, g, b),
+            d = max - min,
+            dX = d !== 0;
+
+        this.hsla = {
             h: dX ? (max === r ? (g - b) / d : max === g ? 2 + ((b - r) / d) : 4 + ((r - g) / d)) * 60 : 0,
             s: dX ? 100 * (v.l > 50 ? d / (2 - max - min) : d / (max + min)) : 0,
             l: 50 * (max + min),
             a: a === undefined ? 1 : a * 255
         };
-    
+
     };
     NPO.prototype.setKind = function (v) {
-    
+
         let oldAble = this.prefs.able;
-        
+
         this.setAble(false);
-        
+
         this.prefs.lizzList = {};
-        
+
         this.prefs.kind = v.split(',');
-    
+
         for (let k in this.prefs.kind) {
-            for (let evt in (K = kindList(this,[this.prefs.kind[k]]))) {
+            let K = kindList(this, [this.prefs.kind[k]]);
+            for (let evt in K) {
                 this.prefs.lizzList[evt] = K[evt];
             }
         }
-        
+
         this.setAble(oldAble);
-    
+
     };
     NPO.prototype.setAble = function (v) {
-    
+
         for (let evt in this.prefs.lizzList) {
             this.obj[((this.prefs.able = v) ? 'add' : 'remove') + 'EventListener'](
                 evt,
                 this.prefs.lizzList[evt], { passive: false }
             );
         }
-    
+
     };
-    NPO.prototype.togAble = function ( ) {
+    NPO.prototype.togAble = function () {
         this.setAble(this.prefs.able ? false : true);
     };
     NPO.prototype.setSens = function (v) {
         this.prefs.sens = v;
     };
-    //}
+    // }
     
-    document.querySelectorAll(defPrefs.selector).forEach(obj => {
-    
-        new NPO(obj);
+    // init when page fully loaded
+    window.addEventListener('load', () => {
         
+        document.querySelectorAll(
+            `[data-${defPrefs.selector.replace(/[^a-z-]/gi, '')}]`
+        ).forEach(obj => {
+    
+            obj.addEventListener('getNosePickerRef', e => {
+                
+                obj.dispatchEvent(new CustomEvent(
+                    'catchNosePickerRef', {
+                        detail : this[obj.id].createRef()
+                    }
+                ));
+                
+            });
+            
+            (this[obj.id] = new NPO(obj)).loadComplete();
+            
+    
+        });
+    
     });
     
-})()});
+})();
