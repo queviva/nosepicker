bfe0bc60da684c5735fb3a64fe0af475b7fe21be
queviva <jabraham.engineer@gmail.com>
2022-01-30 20:28:50 -0800

    demo page of the nanopicker, and adjustment to index to link to the nano demo

--- nosepicker.nano.js
+++ nosepicker.nano.js
@@ -1,1 +1,37 @@
-((T='touches',G=(...v)=>v.sort((a,b)=>a-b)[1],m=[0,0],S=(v,n)=>v.forEach((p,i)=>n.style.setProperty('--nose-'+('HSLA'[i]||'hsla'),p+' %%  '[i])),C=(e,n,v,X,Y,K)=>{e?(e.stopPropagation(),e.preventDefault()):'';K?(v[1]=G(v[1]+Y/50,100,0),v[3]=G(v[3]-X/5000,1,0)):(v[0]-=Y/50,v[2]=G(v[2]+X/100,100,0));S([...v,'hsla('+v.map((p,i)=>p+' %% '[i])+')'],n);},)=>document.querySelectorAll('[data-nose]').forEach((N,v)=>{S(v=(window.getComputedStyle(N).getPropertyValue('--nose-hsla')||'0,100,50,1').match(/[\d\.]+/g).map(v=>Number(v)),N);let Z={'wheel':e=>C(e,N,v,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),'touchstart':e=>m=[e[T][0].pageX,e[T][0].pageY],'touchmove':e=>C(e,N,v,m[0]-e[T][0].pageX,m[1]-e[T][0].pageY,e[T][1])};for(let z in Z){N.addEventListener(z,Z[z])}}))();
+((T='touches',G=(...v)=>v.sort((a,b)=>a-b)[1],m=[0,0],
+
+S=(v,n)=>[...v,'hsla('+v.map((p,i)=>p+' %% '[i])+')']
+.forEach((p,i)=>n.style.setProperty(
+    '--nose-'+('HSLA'[i]||'hsla'),p+' %%  '[i]
+)),
+
+C=(e,n,v,X,Y,K)=>{
+    
+    e.stopPropagation();e.preventDefault();
+
+    K?(v[1]=G(v[1]+Y/50,100,0),v[3]=G(v[3]-X/5000,1,0)):(v[0]-=Y/50,v[2]=G(v[2]+X/100,100,0));
+    
+    S(v,n);
+    
+},
+    
+)=>document.querySelectorAll('[data-nose]').forEach(
+        
+    (N,v
+    ,J
+    
+    )=>{
+
+J=document.getElementById(N.dataset.nose)||N;
+        
+    S(v=(window.getComputedStyle(N).getPropertyValue('--nose-hsla')||'0,100,50,1').match(/[\d\.]+/g).map(p=>Number(p)),N);
+    
+    let Z = {
+        'wheel':e=>C(e,J,v,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),
+        'touchstart':e=>m=[e[T][0].pageX,e[T][0].pageY],
+        'touchmove':e=>C(e,J,v,m[0]-e[T][0].pageX,m[1]-e[T][0].pageY,e[T][1])
+    };
+    
+    for(let z in Z){N.addEventListener(z,Z[z])}
+    
+}))();
