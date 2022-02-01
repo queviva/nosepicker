95a6f4fdfd4ffb5d971569c88fd132ab0a401e10
queviva <jabraham.engineer@gmail.com>
2022-02-01 05:36:13 -0800

    cut out enough characters to add a set-value listener and still be under 800 characters

--- nosepicker.nano.js
+++ nosepicker.nano.js
@@ -14,22 +14,18 @@ document.querySelectorAll('[data-nose]').forEach((
     
     S=()=>
         [...V,H+'('+V.map((p,i)=>p+' %% '[i])+')'].forEach((p,i)=>
-        J.style.setProperty('--nose-'+(H[i]||H),p+' %%  '[i]))
-    ,
+        J.style.setProperty('--nose-'+(H[i]||H),p+' %%  '[i])),
 
-    C=(e,X,Y,K)=>{
-    
-        e.stopPropagation();
-        e.preventDefault();
-    
-        S(K?(V[1]=G(V[1]+Y/50,100,0),V[3]=G(V[3]-X/5000,1,0)):(V[0]-=Y/50,V[2]=G(V[2]+X/100,100,0)));
-    
-    },
+    C=(e,X,Y,K,z=e.stopPropagation()+e.preventDefault())=>S(K?
+        (V[1]=G(V[1]+Y/50,100,0),V[3]=G(V[3]-X/5000,1,0)):
+        (V[0]-=Y/50,V[2]=G(V[2]+X/100,100,0))),
     
     Z = {
         'wheel':e=>C(e,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),
         'touchstart':e=>M=[e[T][0].pageX,e[T][0].pageY],
         'touchmove':e=>C(e,M[0]-e[T][0].pageX,M[1]-e[T][0].pageY,e[T][1])
+        
+        ,'set-val':e=>S(V=e.detail)
     }
     
 )=>{
