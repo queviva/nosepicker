268cca0b67447e24be9250b667452fc4b0cbf0b0
queviva <jabraham.engineer@gmail.com>
2022-02-01 03:53:02 -0800

    added a set-nose listener

--- nosepicker.micro.js
+++ nosepicker.micro.js
@@ -55,7 +55,10 @@
         'wheel':e=>C(e,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),
         'touchstart':e=>M=[e[T][0].pageX,e[T][0].pageY],
         'touchmove':e=>C(e,M[0]-e[T][0].pageX,M[1]-e[T][0].pageY,e[T][1]),
-        'click':e=>D(`${R}-change`,{detail:Q()})
+        'click':e=>D(R+'-change',{detail:Q()}),
+        ['set-'+R]:e=>S(V=e.detail)
+        
+        
     }
     
 )=>{
