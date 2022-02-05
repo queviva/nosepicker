da8b4358faa0b776cf2d0c7f353ce31e48b92b08
queviva <jabraham.engineer@gmail.com>
2022-02-05 13:21:27 -0800

    added a negative to the number converter

--- nosepicker.nano.js
+++ nosepicker.nano.js
@@ -16,7 +16,7 @@ document.querySelectorAll('[data-nose]').forEach((N,V,J,
         (V[1]=G(V[1]+Y/50,100,0),V[3]=G(V[3]-X/5000,1,0)):
         (V[0]-=Y/50,V[2]=G(V[2]+X/100,100,0))),
     
-    Z = {
+    Z={
         'wheel':e=>C(e,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),
         'touchstart':e=>M=[e[T][0].pageX,e[T][0].pageY],
         'touchmove':e=>C(e,M[0]-e[T][0].pageX,M[1]-e[T][0].pageY,e[T][1]),
@@ -24,14 +24,13 @@ document.querySelectorAll('[data-nose]').forEach((N,V,J,
     }
     
 )=>{
-
-        
+    
     S(
         
         J=document.getElementById(N.dataset.nose)||N,
         
         V=(window.getComputedStyle(J).getPropertyValue('--nose-'+H)||
-        '0,100,50,1').match(/[\d\.]+/g).map(p=>Number(p))
+        '0,100,50,1').match(/[\d\.-]+/g).map(p=>Number(p))
         
     );
     
