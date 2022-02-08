87d41ba368e0eccca815f9e02a91cca5397ee9ff
queviva <jabraham.engineer@gmail.com>
2022-02-07 16:14:50 -0800

    takes custome css-variable-name now; still REALLY small

--- nosepicker.nano.js
+++ nosepicker.nano.js
@@ -1,16 +1,16 @@
-document.querySelectorAll('[data-nose]').forEach((N,V,J,
+document.querySelectorAll('[data-nose]').forEach((N,V,J,X,
     
     T='touches',
     
     H='hsla',
     
-    M=[0,0],Q='--nose-',
+    M=[0,0],
     
     G=(...v)=>v.sort((a,b)=>a-b)[1],
     
     S=()=>
         [...V,H+'('+V.map((p,i)=>p+' %% '[i])+')'].forEach((p,i)=>
-        J.style.setProperty(Q+(H[i]||H),p+' %%  '[i])),
+        J.style.setProperty(X+(H[i]||H),p+' %%  '[i])),
 
     C=(e,X,Y,K,z=e.stopPropagation()+e.preventDefault())=>S(K?
         (V[1]=G(V[1]+Y/50,100,0),V[3]=G(V[3]-X/5000,1,0)):
@@ -26,12 +26,12 @@ document.querySelectorAll('[data-nose]').forEach((N,V,J,
 )=>{
     
     S(
+        [J,X]=[N.id,'--nose-'].map((v,i)=>N.dataset.nose.split(' ')[i]||v),
         
-        J=document.getElementById(N.dataset.nose)||N,
+        J=document.getElementById(J)||N,
         
-        V=(window.getComputedStyle(J).getPropertyValue(Q+H)||
+        V=(window.getComputedStyle(J).getPropertyValue(X+H)||
         '0,100,50,1').match(/[\d\.-]+/g).map(p=>Number(p))
-        
     );
     
     for(let z in Z)N.addEventListener(z,Z[z]);
