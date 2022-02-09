085cc81c218b6074c55b0a79a8de079f7fca4857
queviva <jabraham.engineer@gmail.com>
2022-02-09 11:34:30 -0800

    uses the correct variable prefix set from individual data-params

--- nosepicker.micro.js
+++ nosepicker.micro.js
@@ -15,22 +15,22 @@
     
     F=Object.assign({},P,JSON.parse(N.dataset[R]||'{}')),
     
+    X=F.selector,
+    
     G=(...v)=>v.sort((a,b)=>a-b)[1],
     
     H=()=>`hsla(${V.map((p,i)=>p+' %% '[i])})`,
     
     D=(e,d)=>N.dispatchEvent(new CustomEvent(e,d)),
     
-    S=()=>D(R+'-input',{
+    S=()=>D(X+'-input',{
         
         detail:H(),
         
         p:[...V,H()].forEach((p,i)=>
-           J.style.setProperty('--'+R+'-'+('hsla'[i]||'hsla'),p+' %%  '[i])),
+           J.style.setProperty('--'+X+'-'+('hsla'[i]||'hsla'),p+' %%  '[i])),
         
-        i:F.pattern?(N.style.backgroundImage=V[3]<1?
-`repeating-linear-gradient(-45deg,rgba(0,0,0,${0.5-V[3]/2}),rgba(0,0,0,${0.5-V[3]/2}) 10px,transparent 10px,transparent 20px)`:''
-        ):''
+        i:F.pattern?(N.style.backgroundImage=V[3]<1? `repeating-linear-gradient(-45deg,rgba(0,0,0,${0.5-V[3]/2}),rgba(0,0,0,${0.5-V[3]/2}) 10px,transparent 10px,transparent 20px)`:''):''
         
     }),
 
@@ -45,26 +45,21 @@
         'wheel':e=>C(e,e.wheelDeltaX,e.wheelDeltaY,e.ctrlKey),
         'touchstart':e=>M=[e[T][0].pageX,e[T][0].pageY],
         'touchmove':e=>C(e,M[0]-e[T][0].pageX,M[1]-e[T][0].pageY,e[T][1]),
-        'click':e=>D(R+'-change',{detail:H()}),
-        ['set-'+R]:e=>S(V=e.detail),
-        ['tog-'+R]:(e,b=(A=!A))=>{for(let z in Z){
+        'click':e=>D(X+'-change',{detail:H()}),
+        ['set-'+X]:e=>S(V=e.detail),
+        ['tog-'+X]:(e,b=(A=!A))=>{for(let z in Z){
             (!e||!z.match(/-/))?
             N[(b?'add':'remove')+'EventListener'](z,Z[z]):''
         }}
     }
     
-)=>{
-    
-    S(
+)=>S(
         
-        J=document.getElementById(F.root)||N,
+    J=document.getElementById(F.root)||N,
         
-        V=(window.getComputedStyle(J).getPropertyValue('--'+R+'-hsla')||
-        '0,100,50,1').match(/[\d\.]+/g).map(p=>Number(p))
+    V=(window.getComputedStyle(J).getPropertyValue('--'+X+'-hsla')||
+        '0,100,50,1').match(/[\d\.]+/g).map(p=>Number(p)),
         
-    );
-    
-    D(R+'load',Z['tog-'+R]());
-    
-    
-})))();
+    D(X+'load',Z['tog-'+X]())
+        
+))))();
